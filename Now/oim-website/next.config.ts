5be14f68c319e66683a07a2565a0b6904518ee3f
Jesus_Obando <ia.strongmagazine@gmail.com>
2026-04-17 00:13:54 -0400

    All

--- Now/oim-website/next.config.ts
+++ Now/oim-website/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  experimental: {
+    turbo: {
+      root: ".",
+    },
+  },
 };
 
 export default nextConfig;
